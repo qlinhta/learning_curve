a54614a5f8687025f0fd07394186961e21476e87
haanh0811 <hatrn0811@gmail.com>
2023-04-23 22:40:57 +0200

    Create routes and link pages

--- LearningCurve/learningcurveapp/urls.py
+++ LearningCurve/learningcurveapp/urls.py
@@ -4,6 +4,14 @@ from . import views
 urlpatterns = [
     path('login/', views.login, name='login'),
     path('student-path/', views.student_path, name='student-path'),
+    path('student-profile/', views.student_profile, name='student-profile'),
+    path('student-student-quiz-result-details/', views.student_quiz_result_details, name='student-student-quiz-result-details'),
+    path('student-quiz-results/', views.student_quiz_results, name='student-quiz-results'),
+    path('student-take-course/', views.student_take_course, name='student-take-course'),
+    path('student-take-lesson/', views.student_take_lesson, name='student-take-lesson'),
+    path('student-take-quiz/', views.student_take_quiz, name='student-take-quiz'),
+     path('student-quiz-results/', views.student_quiz_results, name='student-quiz-results'),
+    
     path('teacher-profile/', views.teacher_profile, name='teacher-profile'),
     path('aboutus/', views.aboutus, name='aboutus'),
    # path('enter_login/', views.enter_login, name='enter_login'),
