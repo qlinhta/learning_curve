7f9f538a599a6b29553077c247b01ae5eca64b57
Quyen Linh TA <60761870+qlinhta@users.noreply.github.com>
2023-06-17 12:45:37 +0200

    Update :: Clear for all html

--- LearningCurve/learningcurveapp/views.py
+++ LearningCurve/learningcurveapp/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from . import forms
-from django.contrib.auth.models import User,auth,Group
+from django.contrib.auth.models import User, auth, Group
 from django.contrib import messages
 from django.contrib.auth import authenticate
 
@@ -8,96 +8,109 @@ from django.contrib.auth import authenticate
 def courses(request):
     return render(request, 'learningcurveapp/courses.html')
 
+
 def index(request):
     return render(request, 'learningcurveapp/index.html')
 
-def login(request):
 
+def login(request):
     if request.user.is_authenticated:
         return redirect('/learningcurveapp/profile')
     if request.method == 'POST':
         username = request.POST['username']
         password = request.POST['password']
-        print(username,password)
+        print(username, password)
         u = authenticate(username=username, password=password)
         print(u)
         if u:
-            auth.login(request,u)
+            auth.login(request, u)
             return redirect('/learningcurveapp/profile')
 
-
-    return render(request, 'learningcurveapp/login.html', context = {
+    return render(request, 'learningcurveapp/login.html', context={
         'error_message': 'The username or password is wrong, Please retry',
-    } )
+    })
 
 
 def student_path(request):
     if not request.user.is_authenticated:
         return redirect('/learningcurveapp/login')
-    return render(request, 'learningcurveapp/student-path.html',context = {
+    return render(request, 'learningcurveapp/student-path.html', context={
         'username': request.user.username,
     })
+
 
 def student_profile(request):
     if not request.user.is_authenticated:
         return redirect('/learningcurveapp/login')
-    return render(request, 'learningcurveapp/student-profile.html',context = {
+    return render(request, 'learningcurveapp/student-profile.html', context={
         'username': request.user.username,
     })
+
 
 def student_quiz_result_details(request):
     if not request.user.is_authenticated:
         return redirect('/learningcurveapp/login')
-    return render(request, 'learningcurveapp/student-quiz-result-details.html',context = {
+    return render(request, 'learningcurveapp/student-quiz-result-details.html', context={
         'username': request.user.username,
     })
+
+
 def student_quiz_results(request):
     if not request.user.is_authenticated:
         return redirect('/learningcurveapp/login')
-    return render(request, 'learningcurveapp/student-quiz-results.html',context = {
+    return render(request, 'learningcurveapp/student-quiz-results.html', context={
         'username': request.user.username,
     })
+
 
 def student_take_course(request):
     if not request.user.is_authenticated:
         return redirect('/learningcurveapp/login')
-    return render(request, 'learningcurveapp/student-take-course.html',context = {
+    return render(request, 'learningcurveapp/student-take-course.html', context={
         'username': request.user.username,
     })
+
 
 def student_take_lesson(request):
     if not request.user.is_authenticated:
         return redirect('/learningcurveapp/login')
-    return render(request, 'learningcurveapp/student-take-lesson.html',context = {
+    return render(request, 'learningcurveapp/student-take-lesson.html', context={
         'username': request.user.username,
     })
+
+
 def student_take_quiz(request):
     if not request.user.is_authenticated:
         return redirect('/learningcurveapp/login')
-    return render(request, 'learningcurveapp/student-take-quiz.html',context = {
+    return render(request, 'learningcurveapp/student-take-quiz.html', context={
         'username': request.user.username,
     })
+
 
 def teacher_profile(request):
     if not request.user.is_authenticated:
         return redirect('learningcurveapp/login')
-    return render(request, 'learningcurveapp/teacher-profile.html',context = {
+    return render(request, 'learningcurveapp/teacher-profile.html', context={
         'username': request.user.username,
     })
+
 
 def teacher_addcourses(request):
     if not request.user.is_authenticated:
         return redirect('/learningcurveapp/login')
-    return render(request, 'learningcurveapp/teacher-addcourses.html',context = {
+    return render(request, 'learningcurveapp/teacher-addcourses.html', context={
         'username': request.user.username,
     })
+
 
 def teacher_mycourses(request):
     if not request.user.is_authenticated:
         return redirect('/learningcurveapp/login')
-    return render(request, 'learningcurveapp/teacher-mycourses.html',context = {
+    return render(request, 'learningcurveapp/teacher-mycourses.html', context={
         'username': request.user.username,
     })
+
+
 def profile(request):
     if not request.user.is_authenticated:
         return redirect('/learningcurveapp/login')
@@ -114,9 +127,9 @@ def profile(request):
         return redirect('/learningcurveapp/login')
 
 
-
 def aboutus(request):
     return render(request, 'learningcurveapp/aboutus.html')
+
 
 def signup(request):
     if request.user.is_authenticated:
@@ -125,33 +138,32 @@ def signup(request):
         username = request.POST['username']
         email = request.POST['email']
         password = request.POST['password']
-        profile_type=request.POST['profile_type']
+        profile_type = request.POST['profile_type']
         if User.objects.filter(username=username).first():
-            messages.info(request,"The username is already taken, please try another username...")
-            return render(request, 'learningcurveapp/signup.html',context = {
+            messages.info(request, "The username is already taken, please try another username...")
+            return render(request, 'learningcurveapp/signup.html', context={
                 'error_message': "The username is already taken, please try another username...",
             })
-        if 'agree'  not in request.POST:
-            messages.info(request,"You have to agree to create an account")
-            return render(request, 'learningcurveapp/signup.html',context = {
+        if 'agree' not in request.POST:
+            messages.info(request, "You have to agree to create an account")
+            return render(request, 'learningcurveapp/signup.html', context={
                 'error_message': "You have to agree to create an account",
             })
         else:
-            agree=request.POST['agree']
-            user=User.objects.create_user(username=username,email=email, password=password)
-            if(profile_type=='Student'):
+            agree = request.POST['agree']
+            user = User.objects.create_user(username=username, email=email, password=password)
+            if (profile_type == 'Student'):
                 my_group = Group.objects.get(name='student')
                 my_group.user_set.add(user)
 
-                auth.login(request,user)
+                auth.login(request, user)
                 return redirect('/learningcurveapp/student-path')
-            if(profile_type=='Teacher'):
+            if (profile_type == 'Teacher'):
                 my_group = Group.objects.get(name='teacher')
                 my_group.user_set.add(user)
-                auth.login(request,user)
+                auth.login(request, user)
                 return redirect('/learningcurveapp/teacher-profile/')
     return render(request, 'learningcurveapp/signup.html')
-
 
 
 def logout(request):
@@ -159,8 +171,3 @@ def logout(request):
         return render(request, 'learningcurveapp/login.html')
     auth.logout(request)
     return render(request, 'learningcurveapp/login.html')
-
-
-
-
-
