9fd20f9146f3324fe7197d788cedb5db92095642
Quyen Linh TA <60761870+qlinhta@users.noreply.github.com>
2023-06-21 20:11:16 +0200

    Update urls.py

--- LearningCurve/LearningCurve/urls.py
+++ LearningCurve/LearningCurve/urls.py
@@ -21,7 +21,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('learningcurveapp/', include('learningcurveapp.urls')),
+    path('', include('learningcurveapp.urls')),
 ]
 if True:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
