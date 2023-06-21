f8ae2e930c107f2aa3068658deeb711b98b54c9c
Quyen Linh TA <60761870+qlinhta@users.noreply.github.com>
2023-06-21 22:34:35 +0200

    Update settings.py

--- LearningCurve/LearningCurve/settings.py
+++ LearningCurve/LearningCurve/settings.py
@@ -122,7 +122,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
@@ -134,3 +134,5 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # set the url path from which the media can be retreived
 MEDIA_URL = '/media/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
